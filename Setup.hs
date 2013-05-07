28cb59c73d26d30d71bfb1ab726d2662a1917e6b
David Fox <dsf@seereason.com>
2013-05-07 13:48:21 +0000

    Move modules into Language.Haskell.Imports, use pretty printer to format generated imports

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,10 @@
 #!/usr/bin/runhaskell
 
-import Distribution.Simple
+import Distribution.PackageDescription (PackageDescription (executables), Executable(modulePath))
+import Distribution.Simple (postConf, defaultMainWithHooks, simpleUserHooks)
+import Distribution.Simple.LocalBuildInfo (localPkgDescr)
+import Language.Haskell.Imports.Clean (cleanDumpedImports)
 
-main = defaultMainWithHooks simpleUserHooks
+main = defaultMainWithHooks simpleUserHooks {
+         postConf = \ _ _ _ lbi -> cleanDumpedImports lbi
+       }
