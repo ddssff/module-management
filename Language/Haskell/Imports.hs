56db4099c1ba5d05ad5b718d8fcfd8f4ec510559
David Fox <dsf@seereason.com>
2013-05-07 12:59:32 +0000

    Split up pure and IO code

--- Language/Haskell/Imports.hs
+++ Language/Haskell/Imports.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wall #-}
 module Language.Haskell.Imports
     ( cleanDumpedImports
     ) where
 
 import Control.Applicative ((<$>), (<*>))
+import Control.Applicative.Error (Failing(..))
 import Control.Exception (SomeException, try, catch, throw)
-import Data.List (isSuffixOf, isInfixOf, tails, isPrefixOf, findIndex)
+import Data.List (isSuffixOf, isInfixOf, tails, isPrefixOf, findIndex, intercalate)
 import Data.Monoid ((<>))
+import Distribution.PackageDescription (PackageDescription (executables), Executable(modulePath))
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo, localPkgDescr)
 import Language.Haskell.Exts.Extension (Extension(PackageImports))
 import Language.Haskell.Exts.Syntax (Module(..), ImportDecl(..))
 import Language.Haskell.Exts.Parser (ParseMode(..))
 import Language.Haskell.Exts (ParseResult(..), parseFile, parseFileWithMode, defaultParseMode)
+import Prelude hiding (last)
 import System.IO.Error (isDoesNotExistError)
 import System.Directory (getDirectoryContents, removeFile, doesFileExist, renameFile)
 
@@ -20,56 +25,57 @@ import System.Directory (getDirectoryContents, removeFile, doesFileExist, rename
 --    2. The first import must be preceded by a blank line
 --    3. The last import must be followed by a blank line
 --    4. No imports commented out with {- -} appear anywhere
--- It will not operate on files without an explicit export list or on files with no declarations.
-cleanDumpedImports :: [FilePath] -> IO ()
-cleanDumpedImports exePaths =
-    getDirectoryContents "." >>= mapM_ (\ path -> if isSuffixOf ".imports" path then checkImport path else return ())
-    where
-      checkImport importsPath =
-          do result <- parseFileWithMode (defaultParseMode {extensions = [PackageImports] ++ extensions defaultParseMode}) importsPath
-             case result of
-               (ParseOk (Module _ _ _ _ _ newImports _)) ->
-                   do putStrLn ("parsed: " ++ importsPath)
-                      sourcePath <- findSourcePath (map (\ c -> if c == '.' then '/' else c) (dropSuffix ".imports" importsPath) <> ".hs")
-                      importsText <- readFile importsPath
-                      source <- try ((,) <$> parseFile sourcePath <*> readFile sourcePath)
-                      either (\ (e :: SomeException) -> putStrLn (sourcePath ++ ": " ++ show e)) (uncurry (updateSource importsPath newImports importsText sourcePath)) source
-               (ParseOk _) -> putStrLn ("ignored: " ++ importsPath)
-               _ -> putStrLn ("Parse of imports failed: " ++ show result)
-      findSourcePath :: FilePath -> IO FilePath
-      findSourcePath path =
-          doesFileExist path >>= \ exists ->
-              case (exists, matches) of
-                (True, []) -> return path
-                (False, [x]) -> return x
-                (False, []) -> error ("Can't find " ++ path)
-                (_, xs) -> error ("Multiple executables named " ++ path ++ ": " ++ show xs)
-          where
-            matches = filter (isSuffixOf ('/' : path)) exePaths
-      updateSource :: FilePath -> [ImportDecl] -> String -> FilePath -> ParseResult Module -> String -> IO ()
-      updateSource importsPath newImports importsText sourcePath (ParseOk (Module _ _ _ _ (Just _) oldImports (_ : _))) sourceText =
-          if newImports /= oldImports then replaceImports importsPath sourceText importsText sourcePath else return ()
-      -- Assume the import section begins with a blank line and then
-      -- "import" and ends with the a blank line following "\nimport"
-      updateSource importsPath newImports importsText sourcePath _ sourceText =
-          putStrLn ("Invalid source file: " ++ sourcePath)
-      replaceImports :: FilePath -> String -> String -> FilePath -> IO ()
-      replaceImports importsPath text imports sourcePath =
-          let start = findIndex (isPrefixOf "\n\nimport") (tails text)
-              (prefix, rest) = splitAt (maybe 0 id start + 1) text
-              last = maybe 1 (+ 1) (findIndex (not . isInfixOf "\nimport") (tails rest))
-              suffix = drop last rest
-              after = fmap (+ 2) (findIndex (isPrefixOf "\n\n") (tails suffix)) in
-          case after of
-            Nothing -> putStrLn (sourcePath ++ ": no changes")
-            Just after' ->
-                let suffix' = drop after' suffix
-                    text' = prefix ++ "\n" ++ imports ++ "\n" ++ suffix' in
-                if text /= text'
-                then putStrLn (sourcePath ++ ": imports replaced by " ++ importsPath) >>
-                     replaceFile (++ "~") sourcePath text'
-                else putStrLn (sourcePath ++ ": no changes")
+-- It will not operate on files without an explicit export list or on
+-- files with no declarations.  To use it do the following:
+--    1. Set the postConf value in Setup  to:
+--         @\ _ _ _ lbi -> cleanDumpedImports lbi@
+--    1. Add -ddump-minimal-imports to the GHC-Options value in your
+--       cabal file.  Then do a Setup build to generate the .imports
+--       files.
+--    2. Next do a Setup configure to update the source files
+--    3. Do another Setup build to make sure everything still builds.
+--       If there are problems with a module, either edit the source
+--       file or move the backup file (suffix ~) back to where the
+--       original was.
+cleanDumpedImports :: LocalBuildInfo -> IO ()
+cleanDumpedImports lbi = cleanDumpedImports' (map modulePath (executables (localPkgDescr lbi)))
 
+cleanDumpedImports' :: [FilePath] -> IO ()
+cleanDumpedImports' exePaths =
+    getDirectoryContents "." >>= mapM_ (\ path -> if isSuffixOf ".imports" path then checkImport exePaths path else return ())
+
+checkImport :: [FilePath] -> FilePath -> IO ()
+checkImport exePaths importsPath =
+    do result <- parseFileWithMode (defaultParseMode {extensions = [PackageImports] ++ extensions defaultParseMode}) importsPath
+       case result of
+         (ParseOk (Module _ _ _ _ _ newImports _)) ->
+             do sourcePath <- findSourcePath exePaths (moduleToPath importsPath)
+                case sourcePath of
+                  Failure messages -> putStrLn (intercalate ", " messages)
+                  Success sourcePath' ->
+                      do importsText <- readFile importsPath
+                         source <- try ((,) <$> parseFile sourcePath' <*> readFile sourcePath')
+                         either (\ (e :: SomeException) -> putStrLn (sourcePath' ++ ": " ++ show e)) (uncurry (updateSource newImports importsText sourcePath')) source
+         _ -> putStrLn ("Parse of imports failed: " ++ show result)
+
+moduleToPath :: String -> FilePath
+moduleToPath name =
+    map (\ c -> if c == '.' then '/' else c) (dropSuffix ".imports" name) <> ".hs"
+
+findSourcePath :: [FilePath] -> FilePath -> IO (Failing FilePath)
+findSourcePath exePaths path = doesFileExist path >>= return . findSourcePath' exePaths path
+
+updateSource :: [ImportDecl] -> String -> FilePath -> ParseResult Module -> String -> IO ()
+updateSource newImports importsText sourcePath (ParseOk (Module _ _ _ _ (Just _) oldImports (_ : _))) sourceText =
+    if newImports /= oldImports then replaceImports sourceText importsText sourcePath else return ()
+updateSource _ _ sourcePath _ _ =
+    putStrLn ("Invalid source file: " ++ sourcePath)
+
+replaceImports :: String -> String -> FilePath -> IO ()
+replaceImports text imports sourcePath =
+    maybe (return ()) (replaceFile (++ "~") sourcePath) (replaceImports' text imports)
+
+-- | If backup is the identity function you're going to have a bad time.
 replaceFile :: (FilePath -> FilePath) -> FilePath -> String -> IO ()
 replaceFile backup path text =
     remove >> rename >> write
@@ -78,44 +84,35 @@ replaceFile backup path text =
       rename = renameFile path (backup path) `catch` (\ (e :: IOError) -> if isDoesNotExistError e then return () else throw e)
       write = writeFile path text
 
+findSourcePath' :: [FilePath] -> FilePath -> Bool -> Failing FilePath
+findSourcePath' exePaths path exists =
+    case (exists, matches) of
+      (True, []) -> Success path
+      (True, _) -> Success path -- dubious
+      (False, [x]) -> Success x
+      (False, []) -> Failure ["Can't find " ++ path]
+      (False, xs) -> Failure ["Multiple executables named " ++ path ++ ": " ++ show xs]
+    where
+      matches = filter (isSuffixOf ('/' : path)) exePaths
+
+-- Assume the import section begins with a blank line and then
+-- "import" and ends with the a blank line following "\nimport"
+replaceImports' :: String -> String -> Maybe String
+replaceImports' text imports =
+    let start = findIndex (isPrefixOf "\n\nimport") (tails text)
+        (prefix, rest) = splitAt (maybe 0 id start + 1) text
+        last = maybe 1 (+ 1) (findIndex (not . isInfixOf "\nimport") (tails rest))
+        suffix = drop last rest
+        after = fmap (+ 2) (findIndex (isPrefixOf "\n\n") (tails suffix)) in
+    case after of
+      Nothing -> Nothing
+      Just after' ->
+          let suffix' = drop after' suffix
+              text' = prefix ++ "\n" ++ imports ++ "\n" ++ suffix' in
+          if text /= text'
+          then Just text'
+          else Nothing
+
 dropSuffix :: Eq a => [a] -> [a] -> [a]
 dropSuffix suf x =
     if isSuffixOf suf x then take (length x - length suf) x else x
-
-{-
--- ImportDecl { importLoc = SrcLoc {srcFilename = "<unknown>.hs", srcLine = 1, srcColumn = 1}
---            , importModule = ModuleName "Text.PrettyPrint.ANSI.Leijen"
---            , importQualified = False
---            , importSrc = False
---            , importPkg = Nothing
---            , importAs = Nothing
---            , importSpecs = Just (False,[IAbs (Ident "Doc"),IVar (Ident "text")])}
-
-prettyImport :: ImportDecl -> Doc
-prettyImport (ImportDecl { importLoc = loc
-                         , importAs = as
-                         , importQualified = qual
-                         , importModule = ModuleName mod
-                         -- , importSrc = src
-                         , importPkg = pkg
-                         , importSpecs = specs}) =
-    text "import " <>
-    maybe empty (\ name -> text (show name ++ " ")) as <>
-    if qualified then text "qualified " else empty <>
-    text mod <>
-    maybe empty prettySpecList specs
-
-
-prettySpecList :: ImportSpecList -> Doc
-prettySpecList (ImportSpecList op flag specs)
-
-
-prettySpecs :: [ImportSpec] -> Doc
-prettySpecs specs = text "(" <> hsep (text ", ") (map prettySpecList specs) <> text ")"
-
-prettySpec :: ImportSpec -> Doc
-prettySpec (IVar name) = empty
-prettySpec (IAbs Name) = empty
-prettySpec (IThingAll Name) = empty
-prettySpec (IThingWith Name [CName]) = empty
--}
