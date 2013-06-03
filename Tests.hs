171790d18a631a79b287536ca8b687fb3e67e199
David Fox <dsf@seereason.com>
2013-06-03 13:16:32 +0000

    Clean up Tests module

--- Tests.hs
+++ Tests.hs
@@ -1,23 +1,17 @@
 {-# LANGUAGE CPP, ScopedTypeVariables #-}
+{-# OPTIONS -Wall #-}
 
 import Control.Exception (SomeException, try)
-import Data.Default (def)
-import Data.Set (Set)
-import Data.Set.Extra (gFind)
-import Language.Haskell.Exts.Annotated
--- import Language.Haskell.Exts (defaultParseMode, parseFileWithComments, ParseResult(..))
-import Language.Haskell.Exts.Comments (Comment)
-import Language.Haskell.Exts.Annotated.ExactPrint (exactPrint)
-import Language.Haskell.Exts.Annotated.Syntax as Syntax
+import Language.Haskell.Exts.Annotated (defaultParseMode, exactPrint, parseFileWithComments, ParseResult(ParseOk))
 import Language.Haskell.Modules.Cat as Cat (test1, test2)
-import Language.Haskell.Modules.Imports as Imports (tests)
 import Language.Haskell.Modules.Common (untabify, withCurrentDirectory)
 import Language.Haskell.Modules.Fold as Fold (test1)
--- import Language.Haskell.Modules.Move as Move (test2)
+import Language.Haskell.Modules.Imports as Imports (tests)
 import Language.Haskell.Modules.Split as Split (tests)
 import System.Exit (ExitCode(ExitSuccess, ExitFailure), exitWith)
-import Test.HUnit (runTestTT, Test(TestList, TestCase, TestLabel), assertEqual, Counts(..))
+import Test.HUnit (assertEqual, Counts(..), runTestTT, Test(TestList, TestCase, TestLabel))
 
+main :: IO ()
 main =
     do counts <- runTestTT (TestList [TestLabel "Cat1" Cat.test1,
                                       TestLabel "Cat2" Cat.test2,
@@ -41,17 +35,6 @@ withTestData f path = withCurrentDirectory "testdata/original" $
          (Left (e :: SomeException), _) -> error $ "failure: " ++ show e
          (_, Left (e :: SomeException)) -> error $ "failure: " ++ show e
 
--- withTestModule :: (Module -> IO r) -> FilePath -> IO r
-withTestModule f path = withTestData (\ m _ _ -> f m) path
-
-{-
--- foo :: IO (Set Name)
-foo = withTestModule (return . gFind) "Debian/Repo/Package.hs"
-
--- bar :: IO (Set QName)
-bar = withTestModule (return . gFind) "Debian/Repo/Package.hs"
--}
-
 tests = TestList [Main.test1 {-, test2-}]
 
 test1 =
@@ -63,35 +46,3 @@ test1 =
      output)
     where
       test parsed comments text = return (exactPrint parsed comments, text)
-
-{-
-test2 =
-    TestLabel "Annoated Parse" $ TestCase
-    (withTestData test "Debian/Repo/Package.hs" >>= \ parsed ->
-     assertEqual
-     "exactPrint"
-     (Syntax.Module (SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unkonwn>.hs",
-                                                         srcSpanStartLine = 1,
-                                                         srcSpanStartColumn = 1,
-                                                         srcSpanEndLine = 1,
-                                                         srcSpanEndColumn = 1},
-                                  srcInfoPoints = []}) Nothing [] [] [])
-     parsed)
-    where
-      test parsed comments text = return parsed
--}
-
-modify parsed comments =
-    ImportDecl {importAnn = SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 1, srcSpanEndLine = 55, srcSpanEndColumn = 45},
-                                         srcInfoPoints = [SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 1, srcSpanEndLine = 55, srcSpanEndColumn = 7}]},
-                importModule = ModuleName (SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 8, srcSpanEndLine = 55, srcSpanEndColumn = 24}, srcInfoPoints = []}) "System.IO.Unsafe",
-                importQualified = False,
-                importSrc = False,
-                importPkg = Nothing,
-                importAs = Nothing,
-                importSpecs = Just (ImportSpecList (SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 25, srcSpanEndLine = 55, srcSpanEndColumn = 45},
-                                                                 srcInfoPoints = [SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 25, srcSpanEndLine = 55, srcSpanEndColumn = 26},
-                                                                                  SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 44, srcSpanEndLine = 55, srcSpanEndColumn = 45}]})
-                                    False
-                                    [IVar (SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 26, srcSpanEndLine = 55, srcSpanEndColumn = 44}, srcInfoPoints = []})
-                                              (Ident (SrcSpanInfo {srcInfoSpan = SrcSpan {srcSpanFilename = "<unknown>.hs", srcSpanStartLine = 55, srcSpanStartColumn = 26, srcSpanEndLine = 55, srcSpanEndColumn = 44}, srcInfoPoints = []}) "unsafeInterleaveIO")])}
