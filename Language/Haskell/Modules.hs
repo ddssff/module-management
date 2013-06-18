3c3b2d508bb9e54888669ff6aae287908fa20e92
David Fox <dsf@seereason.com>
2013-06-18 14:08:37 +0000

    Add more exports to Language.Haskell.Modules

--- Language/Haskell/Modules.hs
+++ Language/Haskell/Modules.hs
@@ -5,17 +5,25 @@ module Language.Haskell.Modules
     , catModules
     , MonadClean
     , runCleanT
-    , Params(extensions, hsFlags, sourceDirs, removeEmptyImports)
+    , Params(extensions, hsFlags, sourceDirs, moduVerse, removeEmptyImports)
     , modifyParams
     , MonadDryRun(..)
     , quietly
     , noisily
+    , qPutStr
+    , qPutStrLn
+    , Extension(..)
+    , withCurrentDirectory
+    , ModuleName(..)
     ) where
 
+import Language.Haskell.Exts.Extension (Extension(..))
+import Language.Haskell.Exts.Syntax (ModuleName(ModuleName))
 import Language.Haskell.Modules.Cat (catModules)
+import Language.Haskell.Modules.Common (withCurrentDirectory)
 import Language.Haskell.Modules.Fold (foldModule)
 import Language.Haskell.Modules.Imports (cleanImports)
 import Language.Haskell.Modules.Split (splitModule)
 import Language.Haskell.Modules.Params (Params(..), MonadClean, runCleanT, modifyParams)
 import Language.Haskell.Modules.Util.DryIO (MonadDryRun(..))
-import Language.Haskell.Modules.Util.QIO (quietly, noisily)
+import Language.Haskell.Modules.Util.QIO (quietly, noisily, qPutStr, qPutStrLn)
