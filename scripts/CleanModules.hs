7c408b8eeebeda85c8bd936df6d4d2240bfba8a4
"Clifford Beshers <beshers@seereason.com>
2013-06-21 19:29:50 +0000

    Minimal cli driver for module import cleaning.

---
+++ scripts/CleanModules.hs
@@ -0,0 +1,20 @@
+#!/usr/bin/env runghc
+
+module Main where
+
+import Control.Monad.Trans (MonadIO(liftIO))
+import Language.Haskell.Modules (cleanImports, runCleanT)
+import System.Directory (getDirectoryContents)
+import System.Environment (getArgs)
+import System.FilePath (takeExtension)
+
+main = do
+  args <- getArgs
+  fs <- if null args then sourceFiles "." else return args
+  cs <- runCleanT $ mapM cleanImports' fs
+  print cs
+
+cleanImports' fp = do (liftIO $ putStr "cleaning: " >> print fp) >> cleanImports fp
+
+sourceFiles :: FilePath -> IO [FilePath]
+sourceFiles fp = getDirectoryContents fp >>= return . filter ((".hs" ==) . takeExtension)
