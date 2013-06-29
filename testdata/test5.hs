681deeaecd798805bce6014d77a7df812a425b8f
David Fox <dsf@seereason.com>
2013-06-29 18:58:15 +0000

    Add testdata to cabal Data-Files field, add a missing test input

---
+++ testdata/test5.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables #-}
+module Data.Logic.Classes.Literal where
+
+a = 1 where
+
+{- This makes bad things happen. -}
+
+b = ()
