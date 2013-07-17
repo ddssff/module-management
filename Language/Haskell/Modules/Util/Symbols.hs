c9388c44814246a8de0920815c5fd85f8a810f70
David Fox <dsf@seereason.com>
2013-07-17 17:21:20 +0000

    Remove apparently unused import

--- Language/Haskell/Modules/Util/Symbols.hs
+++ Language/Haskell/Modules/Util/Symbols.hs
@@ -14,7 +14,6 @@ import Data.Set as Set (empty, insert, Set, toList)
 import Language.Haskell.Exts.Annotated.Simplify (sName)
 import qualified Language.Haskell.Exts.Annotated.Syntax as A (ClassDecl(..), ConDecl(..), Decl(..), DeclHead(..), ExportSpec(..), FieldDecl(..), GadtDecl(..), ImportSpec(..), InstHead(..), Match(..), Name, Pat(..), PatField(..), QName(..), QualConDecl(..), RPat(..))
 import qualified Language.Haskell.Exts.Syntax as S (CName(..), ExportSpec(..), ImportSpec(..), Name(..), QName(..))
-import Language.Haskell.Modules.Util.SrcLoc ()
 
 -- | Do a fold over the names that are declared in a declaration (not
 -- every name that appears, just the ones that the declaration is
