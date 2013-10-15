be4f2e9d24b46a300dcf4e4702c993f3db1cab5f
David Fox <dsf@seereason.com>
2013-10-15 14:45:00 +0000

    version 0.17.3 - make changelog visible in hackage

--- Setup.hs
+++ Setup.hs
@@ -6,10 +6,11 @@ import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
 -- import Language.Haskell.Imports.Params (runParamsT)
 import System.Cmd (system)
 import System.Exit (ExitCode(ExitSuccess))
-import System.Directory (setCurrentDirectory)
+import System.Directory (setCurrentDirectory, copyFile)
 import System.FilePath ((</>))
 
-main = defaultMainWithHooks simpleUserHooks
+main = copyFile "debian/changelog" "changelog" >>
+       defaultMainWithHooks simpleUserHooks
          { sDistHook = \ p l u f ->
              system "tar cfz testdata.tar.gz testdata" >> (sDistHook simpleUserHooks) p l u f
          -- , postBuild = \ _ _ _ lbi -> do code <- system (buildDir lbi </> "tests/tests")
